6152d78f04b082b8b2aab99630d6c46f1ab4cd46
Porrith Suong <psuong95@gmail.com>
2016-12-23 13:24:06 -0500

    Removes classmethod decorator.

--- etf_index.py
+++ etf_index.py
@@ -1,10 +1,9 @@
 class ETFIndex:
-	def __init__(self, cursor):
-		self.cursor = cursor
+	def __init__(self, dbcursor):
+		self.cursor = dbcursor
 		self.trading_symbols = self.get_trading_symbols()
 
-	@classmethod
-	def get_trading_symbols(self, cursor, industry=7):
+	def get_trading_symbols(self, industry=7):
 		"""
 		Gets 50 trading symbols from a sql query, these will be used to compute
 		the ETF index
@@ -12,14 +11,13 @@ class ETFIndex:
 		sql_statement = "select TRADING_SYMBOL from STOCK_HISTORY where \
 						INSTRUMENT_ID in (select INSTRUMENT_ID from INSTRUMENT \
 						where SCND_IDST_CLS_ID=%s) order by rand() limit 50;" % (industry)
-		cursor.execute(sql_statement)
-		query = cursor.fetchall()
+		self.cursor.execute(sql_statement)
+		query = self.cursor.fetchall()
 		trade_symbols = []
 		for item in query:
 			trade_symbols.append(item["TRADING_SYMBOL"])
 		return trade_symbols
 
-	@classmethod
 	def compute_etf_index(self, cursor, industry):
 		"""
 		:param cursor: mysqldb object
