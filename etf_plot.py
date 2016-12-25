1a3c6ffce144740a9182d94d7bd898ca2c378eb4
Satya Gupta <sgupta04@citymail.cuny.edu>
2016-12-25 12:01:56 -0500

    removed nav functionality

--- etf_plot.py
+++ etf_plot.py
@@ -5,22 +5,19 @@ import numpy as np
 
 def plot(cursor):
 	for etf_name in ["IND","PHA","MED","FIN"]:
-		sql = "select TRADE_DATE, CLOSE_PRICE, ULYING_INDEX, NAV from EXCH_TRD_FUND where TRADING_SYMBOL like %s"
+		sql = "select TRADE_DATE, CLOSE_PRICE, ULYING_INDEX from EXCH_TRD_FUND where TRADING_SYMBOL like %s"
 		cursor.execute(sql,etf_name)
 		query = cursor.fetchall()
 		value = []
 		index = []
-		nav   = []
 		date  = []
 		for item in query:
 			date.append(item['TRADE_DATE'])
 			value.append(float(item['CLOSE_PRICE']))
 			index.append(float(item['ULYING_INDEX']))
-			nav.append(float(item['NAV']))
 		#print(value,index,nav,date)
 		plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
 		#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 		plt.plot(date, value)
 		plt.plot(date, index)
-		plt.plot(date, nav)
 		plt.show()
